19bfb3ba75f1cf719bf8bed9a8f5c3bdaee6bc7e
Cyril <Cyril@Cyril-PC>
2014-12-04 10:04:16 +0100

    Second commit

--- src/cyril/Cyril.java
+++ src/cyril/Cyril.java
@@ -5,6 +5,7 @@ public class Cyril {
     public static void main(String[] args) {
         System.out.println("Hello World");
         String nom="Jacky";
+        System.out.println("Test");
         int age= 22;
         int i = 0;
         Lapin JackyLeLapin = new Lapin("Jacky",22);
